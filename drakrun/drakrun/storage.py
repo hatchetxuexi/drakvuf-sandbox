9cb20d538d5dbb0e2719851e785429ee0399aa59
Hubert Jasudowicz <hubert.jasudowicz@cert.pl>
2020-07-14 11:25:54 +0200

    reformat and add comments

--- drakrun/drakrun/storage.py
+++ drakrun/drakrun/storage.py
@@ -4,69 +4,112 @@ import shlex
 
 from typing import Any
 
+
 class StorageBackendBase:
-    def __init__(self, install_info):
+    """Base class for all storage backends
+
+    Defines interface that has to be implemented in order to be
+    considered a valid storage backend for DRAKVUF sandbox.
+
+    In DRAKVUF Sandbox, worker virtual machines are named from
+    vm-1 to vm-[n] - as configured by max_vms parameter during setup.
+
+    vm-0 is considered special as it is used as a base for running
+    other machines. The steps from creating vm-0 to running a worker are:
+    create vm-0 -> configure vm-0 -> snapshot vm-0 -> restore vm-0 as vm-[i]
+    """
+
+    def __init__(self, install_info: Dict[str, Any]):
         self._install_info = install_info
 
     def initialize_vm0_volume(self, disk_size: str):
-        raise NotImplementedError()
+        """Create base volume for VM-0 with given size
 
-    def snapshot_vm0_vlume(self):
-        raise NotImplementedError()
+        disk_size - string representing volume size with M/G/T suffix, eg. 100G
+        """
+        raise NotImplementedError
+
+    def snapshot_vm0_volume(self):
+        """ Save or snapshot base VM-0 volume for later use by other VMs """
+        raise NotImplementedError
 
     def get_vm_disk_path(self, vm_id: int) -> str:
-        raise NotImplementedError()
+        """ Returns disk path for given VM as defined by XL configuration """
+        raise NotImplementedError
 
     def rollback_vm_storage(self, vm_id: int):
-        raise NotImplementedError()
+        """ Rolls back changes and prepares fresh storage for new run of this VM """
+        raise NotImplementedError
 
 
 class ZfsStorageBackend(StorageBackendBase):
     def __init__(self):
         self.check_tools()
-    
+
     def check_tools(self):
         try:
             subprocess.check_output("zfs -?", shell=True)
         except subprocess.CalledProcessError:
-            logging.exception("Failed to execute zfs command. Make sure you have ZFS support installed.")
+            logging.exception(
+                "Failed to execute zfs command. Make sure you have ZFS support installed."
+            )
             return
 
     def initialize_vm0_volume(self, disk_size):
-        vm0_vol = shlex.quote(os.path.join(zfs_tank_name, 'vm-0'))
+        vm0_vol = shlex.quote(os.path.join(zfs_tank_name, "vm-0"))
         try:
-            subprocess.check_output(f"zfs destroy -Rfr {vm0_vol}", stderr=subprocess.STDOUT, shell=True)
+            subprocess.check_output(
+                f"zfs destroy -Rfr {vm0_vol}", stderr=subprocess.STDOUT, shell=True
+            )
         except subprocess.CalledProcessError as e:
-            if b'dataset does not exist' not in e.output:
-                logging.exception(f"Failed to destroy the existing ZFS volume {vm0_vol}.")
+            if b"dataset does not exist" not in e.output:
+                logging.exception(
+                    f"Failed to destroy the existing ZFS volume {vm0_vol}."
+                )
         try:
-            subprocess.check_output(' '.join([
-                "zfs",
-                "create",
-                "-V",
-                shlex.quote(disk_size),
-                shlex.quote(os.path.join(zfs_tank_name, 'vm-0'))
-            ]), shell=True)
+            subprocess.check_output(
+                " ".join(
+                    [
+                        "zfs",
+                        "create",
+                        "-V",
+                        shlex.quote(disk_size),
+                        shlex.quote(os.path.join(zfs_tank_name, "vm-0")),
+                    ]
+                ),
+                shell=True,
+            )
         except subprocess.CalledProcessError:
             logging.exception("Failed to create a new volume using zfs create.")
             return
 
     def snapshot_vm0_volume(self):
-        snap_name = shlex.quote(os.path.join(install_info["zfs_tank_name"], 'vm-0@booted'))
-        subprocess.check_output(f'zfs snapshot {snap_name}', shell=True)
-        
+        snap_name = shlex.quote(
+            os.path.join(install_info["zfs_tank_name"], "vm-0@booted")
+        )
+        subprocess.check_output(f"zfs snapshot {snap_name}", shell=True)
+
     def get_vm_disk_path(self, vm_id: int) -> str:
         zfs_tank_name = self._install_info["zfs_tank_name"]
-        return f'phy:/dev/zvol/{zfs_tank_name}/vm-{vm_id},hda,w'
+        return f"phy:/dev/zvol/{zfs_tank_name}/vm-{vm_id},hda,w"
 
     def rollback_vm_storage(self, vm_id: int):
-        vm_zvol = os.path.join('/dev/zvol', install_info['zfs_tank_name'], f'vm-{vm_id}')
-        vm_snap = os.path.join(install_info['zfs_tank_name'], f'vm-{vm_id}@booted')
+        vm_zvol = os.path.join(
+            "/dev/zvol", install_info["zfs_tank_name"], f"vm-{vm_id}"
+        )
+        vm_snap = os.path.join(install_info["zfs_tank_name"], f"vm-{vm_id}@booted")
 
         if not os.path.exists(vm_zvol):
-            subprocess.run(["zfs", "clone",
-                            "-p", os.path.join(install_info['zfs_tank_name'], 'vm-0@booted'),
-                            os.path.join(install_info['zfs_tank_name'], f'vm-{vm_id}')], check=True)
+            subprocess.run(
+                [
+                    "zfs",
+                    "clone",
+                    "-p",
+                    os.path.join(install_info["zfs_tank_name"], "vm-0@booted"),
+                    os.path.join(install_info["zfs_tank_name"], f"vm-{vm_id}"),
+                ],
+                check=True,
+            )
 
             for _ in range(120):
                 if not os.path.exists(vm_zvol):
@@ -74,12 +117,15 @@ class ZfsStorageBackend(StorageBackendBase):
                 else:
                     break
             else:
-                logging.error(f'Failed to see {vm_zvol} created after executing zfs clone command.')
+                logging.error(
+                    f"Failed to see {vm_zvol} created after executing zfs clone command."
+                )
                 return
 
             subprocess.run(["zfs", "snapshot", vm_snap], check=True)
 
         subprocess.run(["zfs", "rollback", vm_snap], check=True)
+
 
 class Qcow2StorageBackend(StorageBackendBase):
     def __init__(self):
@@ -89,19 +135,26 @@ class Qcow2StorageBackend(StorageBackendBase):
         try:
             subprocess.check_output("qemu-img --version", shell=True)
         except subprocess.CalledProcessError as e:
-            logging.exception("Failed to determine qemu-img version. Make sure you have qemu-utils installed.")
+            logging.exception(
+                "Failed to determine qemu-img version. Make sure you have qemu-utils installed."
+            )
             raise e
 
     def initialize_vm0_volume(self):
         try:
-            subprocess.check_output(' '.join([
-                "qemu-img",
-                "create",
-                "-f",
-                "qcow2",
-                os.path.join(LIB_DIR, "volumes/vm-0.img"),
-                shlex.quote(disk_size)
-            ]), shell=True)
+            subprocess.check_output(
+                " ".join(
+                    [
+                        "qemu-img",
+                        "create",
+                        "-f",
+                        "qcow2",
+                        os.path.join(LIB_DIR, "volumes/vm-0.img"),
+                        shlex.quote(disk_size),
+                    ]
+                ),
+                shell=True,
+            )
         except subprocess.CalledProcessError:
             logging.exception("Failed to create a new volume using qemu-img.")
             return
@@ -109,21 +162,30 @@ class Qcow2StorageBackend(StorageBackendBase):
     def snapshot_vm0_volume(self):
         # We'll be using vm-0.img as backing storage
         pass
-        
 
     def get_vm_disk_path(self, vm_id: int) -> str:
-        return f'tap:qcow2:{LIB_DIR}/volumes/vm-{vm_id}.img,xvda,w'
+        return f"tap:qcow2:{LIB_DIR}/volumes/vm-{vm_id}.img,xvda,w"
 
     def rollback_vm_storage(self, vm_id: int):
         try:
-            os.unlink(os.path.join(LIB_DIR, "volumes/vm-{vm_id}.img".format(vm_id=vm_id)))
+            os.unlink(
+                os.path.join(LIB_DIR, "volumes/vm-{vm_id}.img".format(vm_id=vm_id))
+            )
         except FileNotFoundError:
             pass
-        
-        subprocess.run(["qemu-img", "create",
-                        "-f", "qcow2",
-                        "-o", "backing_file=vm-0.img",
-                        os.path.join(LIB_DIR, "volumes/vm-{vm_id}.img".format(vm_id=vm_id))], check=True)
+
+        subprocess.run(
+            [
+                "qemu-img",
+                "create",
+                "-f",
+                "qcow2",
+                "-o",
+                "backing_file=vm-0.img",
+                os.path.join(LIB_DIR, "volumes/vm-{vm_id}.img".format(vm_id=vm_id)),
+            ],
+            check=True,
+        )
 
 
 def get_storage_backend(install_info: Dict[str, Any]) -> StorageBackendBase:
@@ -135,4 +197,4 @@ def get_storage_backend(install_info: Dict[str, Any]) -> StorageBackendBase:
     }
 
     return backends[backend_name](install_info)
-    
+
